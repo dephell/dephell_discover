ffe0fcd15a3b4ea56b027ffd6f7866e661c7f64a
Gram <master_fess@mail.ru>
2019-03-20 22:21:07 +0300

    +tests

---
+++ tests/test_package.py
@@ -0,0 +1,22 @@
+from dephell_package import Package
+
+
+def test_discover_packages(tmp_path):
+    path = tmp_path / 'project1'
+    p = Package(path=path)
+
+    path.mkdir()
+    (path / 'dir1').mkdir()
+    (path / 'dir2').mkdir()
+    (path / 'empty').mkdir()
+    (path / '__pycache__').mkdir()
+
+    (path / '__init__.py').touch()
+    (path / 'file1.py').touch()
+    (path / 'file2.json').touch()
+    (path / 'dir1' / '__init__.py').touch()
+    (path / 'dir2' / 'file3.json').touch()
+    (path / '__pycache__' / 'lol.pyc').touch()
+
+    assert set(p.packages) == {'project1', 'project1.dir1'}
+    assert set(p.data['project1']) == {'dir2/*'}
