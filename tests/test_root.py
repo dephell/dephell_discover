b2c76bcd0e054378fde8a8a0cc852b03f834222c
Gram <master_fess@mail.ru>
2019-04-07 16:25:42 +0300

    test packages

--- tests/test_root.py
+++ tests/test_root.py
@@ -39,38 +39,61 @@ def test_discover_packages(tmp_path):
     assert data['project1.dir3'] == {'dir4/*.json'}
 
 
-@pytest.mark.parametrize('name, files, expected', [
-    ['foobar', ('foobar/__init__.py', 'foobar/foo.py', 'foobar/bar.py'), {'': ''}],
-    ['foobar', ('src/__init__.py', 'src/foo.py', 'src/bar.py'), {'foobar': 'src'}],
-    ['foobar', ('src/foo.py', 'src/bar.py'), {'': 'src'}],
-    ['foobar', ('__init__.py', 'foo.py', 'bar.py'), {'foobar': ''}],
-    ['foobar', ('src/foobar/__init__.py', 'src/foobar/foo.py', 'src/foobar/bar.py'), {'': 'src'}],
+@pytest.mark.parametrize('files, expected', [
+    [('foobar/__init__.py', 'foobar/foo.py', 'foobar/bar.py'), {'': ''}],
+    [('src/__init__.py', 'src/foo.py', 'src/bar.py'), {'foobar': 'src'}],
+    [('src/foo.py', 'src/bar.py'), {'': 'src'}],
+    [('__init__.py', 'foo.py', 'bar.py'), {'foobar': ''}],
+    [('src/foobar/__init__.py', 'src/foobar/foo.py', 'src/foobar/bar.py'), {'': 'src'}],
 ])
-def test_package_dir(name, files, expected, tmp_path):
+def test_package_dir(files, expected, tmp_path):
     for file_path in files:
         path = tmp_path.joinpath(file_path)
         if '/' in file_path:
             path.parent.mkdir(parents=True, exist_ok=True)
         path.touch()
 
-    root = Root(path=tmp_path, name=name)
+    root = Root(path=tmp_path, name='foobar')
     assert root.package_dir == expected
 
 
-@pytest.mark.parametrize('name, files, expected', [
-    ['foobar', ('foobar/__init__.py', 'foobar/foo.py', 'foobar/bar.py'), {'foobar/': 'foobar'}],
-    ['foobar', ('src/__init__.py', 'src/foo.py', 'src/bar.py'), {'src/': 'foobar'}],
-    ['foobar', ('src/foo.py', 'src/bar.py'), {'src/': ''}],
-    ['foobar', ('__init__.py', 'foo.py', 'bar.py'), {'': 'foobar'}],
-    ['foobar', ('src/foobar/__init__.py', 'src/foobar/foo.py', 'src/foobar/bar.py'), {'src/foobar/': 'foobar'}],
+@pytest.mark.parametrize('files, expected', [
+    [('foobar/__init__.py', 'foobar/foo.py', 'foobar/bar.py'), {'foobar/': 'foobar'}],
+    [('src/__init__.py', 'src/foo.py', 'src/bar.py'), {'src/': 'foobar'}],
+    [('src/foo.py', 'src/bar.py'), {'src/': ''}],
+    [('__init__.py', 'foo.py', 'bar.py'), {'': 'foobar'}],
+    [('src/foobar/__init__.py', 'src/foobar/foo.py', 'src/foobar/bar.py'), {'src/foobar/': 'foobar'}],
 ])
-def test_get_module_name(name, files, expected, tmp_path):
+def test_get_module_name(files, expected, tmp_path):
     for file_path in files:
         path = tmp_path.joinpath(file_path)
         if '/' in file_path:
             path.parent.mkdir(parents=True, exist_ok=True)
         path.touch()
 
-    root = Root(path=tmp_path, name=name)
+    root = Root(path=tmp_path, name='foobar')
     for file_path, module_name in expected.items():
         assert root._get_module_name(path=tmp_path.joinpath(file_path)) == module_name
+
+
+@pytest.mark.parametrize('files, expected_modules', [
+    [('foobar/__init__.py', 'foobar/foo.py', 'foobar/bar.py'), {'foobar'}],
+    [('src/__init__.py', 'src/foo.py', 'src/bar.py'), {'foobar'}],
+    # [('src/foo.py', 'src/bar.py'), {''}],
+    [('__init__.py', 'foo.py', 'bar.py'), {'foobar'}],
+    [('src/foobar/__init__.py', 'src/foobar/foo.py', 'src/foobar/bar.py'), {'foobar'}],
+    [
+        ('src/foobar/__init__.py', 'src/foobar/foo.py', 'src/foobar/sub/bar.py', 'src/foobar/sub/__init__.py'),
+        {'foobar', 'foobar.sub'},
+    ],
+])
+def test_packages(files, expected_modules, tmp_path):
+    for file_path in files:
+        path = tmp_path.joinpath(file_path)
+        if '/' in file_path:
+            path.parent.mkdir(parents=True, exist_ok=True)
+        path.touch()
+
+    root = Root(path=tmp_path, name='foobar')
+    assert len(root.packages) == len(expected_modules)
+    assert {package.module for package in root.packages} == expected_modules
