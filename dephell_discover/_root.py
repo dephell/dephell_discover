39f05fad0af57ed2db272a099a5cd5c6e48d0a79
Gram <master_fess@mail.ru>
2019-04-07 15:16:55 +0300

    +package_dir finder

--- dephell_discover/_root.py
+++ dephell_discover/_root.py
@@ -1,6 +1,6 @@
 from itertools import chain
 from pathlib import Path
-from typing import List, Set, Optional
+from typing import Dict, List, Set, Optional
 
 import attr
 
@@ -13,35 +13,33 @@ from ._data import Data
 @attr.s()
 class Root:
     path = attr.ib(type=Path)
+    name = attr.ib(type=str, default=None)
 
-    @property
-    def name(self):
-        return self.path.name
+    def __attrs_post_init__(self):
+        if self.name is None:
+            self.name = self.path.name
 
-    def _make_data(self, path: Path, ext: str) -> Optional[Data]:
-        paths = {package.path for package in self.packages}
-        for parent in chain((path,), path.parents):
-            if parent not in paths:
-                continue
-            return Data(path=path, ext=ext, package=Package(path=parent, root=self.path))
-        # data not in any package
-        return None
+    # properties
 
-    def include(self, path: Path) -> bool:
-        parts = path.parts[len(self.path.parts):]
-        # hidden dirs and files in root dir
-        if parts[0][0] == '.':
-            return False
-        # bad dirs in the root of the project
-        if parts[0] in BAD_DIRS_ROOT:
-            return False
-        # bad dirs inside the project
-        if set(parts) & BAD_DIRS_ANY:
-            return False
-        # bad extension
-        if path.suffix.replace('.', '') in BAD_EXTS:
-            return False
-        return True
+    @cached_property
+    def package_dir(self) -> Dict[str, str]:
+        """
+        https://docs.python.org/2/distutils/examples.html#pure-python-distribution-by-package
+        """
+        # process cases with `src` dir
+        path = self.path / 'src'
+        if path.exists():
+            if (path / '__init__.py').exists():
+                return {self.name: 'src'}
+            else:
+                return {'': 'src'}
+
+        # when package placed in the root without separated dir
+        if (self.path / '__init__.py').exists():
+            return {self.name: ''}
+
+        # when packages placed in the dirs by package names
+        return {'': ''}
 
     @cached_property
     def packages(self) -> List[Package]:
@@ -64,3 +62,32 @@ class Root:
             if data is not None:
                 result.add(data)
         return result
+
+    # public methods
+
+    def include(self, path: Path) -> bool:
+        parts = path.parts[len(self.path.parts):]
+        # hidden dirs and files in root dir
+        if parts[0][0] == '.':
+            return False
+        # bad dirs in the root of the project
+        if parts[0] in BAD_DIRS_ROOT:
+            return False
+        # bad dirs inside the project
+        if set(parts) & BAD_DIRS_ANY:
+            return False
+        # bad extension
+        if path.suffix.replace('.', '') in BAD_EXTS:
+            return False
+        return True
+
+    # private methods
+
+    def _make_data(self, path: Path, ext: str) -> Optional[Data]:
+        paths = {package.path for package in self.packages}
+        for parent in chain((path,), path.parents):
+            if parent not in paths:
+                continue
+            return Data(path=path, ext=ext, package=Package(path=parent, root=self.path))
+        # data not in any package
+        return None
