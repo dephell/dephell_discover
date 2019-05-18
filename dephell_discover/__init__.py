16ab0bf6bf8814236a2eca799f2d3c10d6c4f1ef
Gram <master_fess@mail.ru>
2019-05-18 15:06:32 +0200

    +docstring for package

--- dephell_discover/__init__.py
+++ dephell_discover/__init__.py
@@ -1,3 +1,9 @@
+"""Discover python packages.
+
++ Find project modules and data files (packages and package_data for setup.py).
++ Extract metainfo.
+"""
+
 from ._data import Data
 from ._package import Package
 from ._root import Root
